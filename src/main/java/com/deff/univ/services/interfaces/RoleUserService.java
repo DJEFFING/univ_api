13cc87c20b519f9c6423f808670f67339dc5721d
DJEFFING <tsafackjefferson2001@gmail.com>
2024-01-22 20:55:43 +0100

    de nouvel mise à jour

--- src/main/java/com/deff/univ/services/interfaces/RoleUserService.java
+++ src/main/java/com/deff/univ/services/interfaces/RoleUserService.java
@@ -1,5 +1,16 @@
 package com.deff.univ.services.interfaces;
 
+import com.deff.univ.models.Role;
+import com.deff.univ.models.RoleUser;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface RoleUserService {
+    public RoleUser save(RoleUser roleUser);
+    public List<Role> findAll();
+    public RoleUser update(int id,RoleUser roleUser);
+    public Optional<RoleUser> findById(int id);
+    public String deleteById();
 
 }
--- src/main/java/com/deff/univ/services/interfaces/SpecialityService.java
+++ src/main/java/com/deff/univ/services/interfaces/SpecialityService.java
@@ -1,5 +1,16 @@
 package com.deff.univ.services.interfaces;
 
+import com.deff.univ.models.Speciality;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface SpecialityService {
+    public Speciality save(Speciality speciality);
+    public List<Speciality> findAll();
+    public  Speciality update(int id,Speciality speciality);
+    public Optional<Speciality> findById(int id);
+    public String deleteById(int id);
+    public  List<Speciality> findByIdTrCenter(int idTrCenter);
 
 }
