bd605c46b56c9b13edaf57b2fe30503f0e032dec
DJEFFING <tsafackjefferson2001@gmail.com>
2024-01-22 15:10:01 +0100

    de nouvel mise à jour

--- src/main/java/com/deff/univ/services/interfaces/EtablishmentService.java
+++ src/main/java/com/deff/univ/services/interfaces/EtablishmentService.java
@@ -1,5 +1,14 @@
 package com.deff.univ.services.interfaces;
 
-public interface EtablishmentService {
+import com.deff.univ.models.Etablishment;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface EtablishmentService {
+    public Etablishment save(Etablishment etablishment);
+    public List<Etablishment> findAll();
+    public Etablishment update(int id,Etablishment etablishment);
+    public Optional<Etablishment> findById(int id);
+    public  String deleteById(int id);
 }
